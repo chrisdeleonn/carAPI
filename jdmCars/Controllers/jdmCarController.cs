c4865e05071cae87fe30c202b5562b6c4aca9728
Chris De Leon <cdeleon.ca91@gmail.com>
2022-10-26 16:58:48 -0400

    added GET & POST

---
+++ jdmCars/Controllers/jdmCarController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace jdmCarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class jdmCarController : Controller
+    {
+        private static List<jdmCar> cars = new List<jdmCar>
+            {
+                new jdmCar {
+                    Id = 1,
+                    Name = "Mazda",
+                    Country = "Japan",
+                    Model = "RX-7",
+                    Year = "1991"}
+        };
+        // 
+        [HttpGet]
+        public async Task<ActionResult<List<jdmCar>>> Get()
+        {
+   
+            return Ok(cars);
+        }
+        //
+        [HttpPost]
+        public async Task<ActionResult<List<jdmCar>>> AddCar(jdmCar car)
+        {
+            cars.Add(car);
+            return Ok(cars);
+        }
+    }
+}
---
+++ jdmCars/jdmCar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace jdmCarAPI
+{
+    public class jdmCar
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Year { get; set; } = string.Empty;
+
+        public string Model { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+
+
+    }
+}
+    
+
